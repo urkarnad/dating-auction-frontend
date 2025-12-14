49150e76f1c3cbc8f7466ae3dbcc2ee0e181dad0
kinetique <sonyacat0423@gmail.com>
2025-12-14 13:48:40 +0200

    added methods responses

--- src/api/lots.js
+++ src/api/lots.js
@@ -35,14 +35,42 @@ export const addMyLotComment = async (data) => {
     return response.data;
 };
 
-export const uploadLotPhoto = async (file) => {
+export const uploadLotPhoto = async (files) => {
     const formData = new FormData();
-    formData.append('photo', file);
+
+    if (Array.isArray(files)) {
+        files.forEach(file => formData.append('photo', file));
+    } else {
+        formData.append('photo', files);
+    }
 
     const response = await api.post('/mylot/upload-photo/', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
     });
 
+    return response.data;
+};
+
+export const deleteLotPhoto = async (photoId) => {
+    const response = await api.delete('/mylot/upload-photo/', {
+        data: { photo_id: photoId }
+    });
+    return response.data;
+};
+
+export const uploadProfilePhoto = async (file) => {
+    const formData = new FormData();
+    formData.append('photo', file);
+
+    const response = await api.post('/profile/upload-photo/', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+    });
+
+    return response.data;
+};
+
+export const deleteProfilePhoto = async () => {
+    const response = await api.delete('/profile/upload-photo/');
     return response.data;
 };
 
